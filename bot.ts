16d71e3759768b8ef8d366caf6096c7c2f4e6c12
Nick Lamicela <nupanick@gmail.com>
2023-04-23 20:49:30 -0700

    Idea for how to force markdown to work with sharepoint html

--- bot.ts
+++ bot.ts
@@ -1,11 +1,37 @@
-const print = console.log;
+type Quit = { type: 'quit' };
+type Say = { type: 'say', text: string };
+type Action = Quit | Say;
 
-function main() {
+type Message = { type: 'message', text: string };
+type Event = Message;
+
+type Bot = (e: Event) => Action[];
+
+const pingBot: Bot = (event) => {
+    if (event.type === 'message') {
+        if (event.text === 'exit') return [{ type: 'quit' }];
+        if (event.text === 'quit') return [{ type: 'quit' }];
+        if (event.text === 'ping') return [{ type: 'say', text: 'pong' }];
+        return [{ type: 'say', text: event.text }];
+    }
+    return [];
+}
+
+const runConsole = (bot: Bot) => {
+    const actions: Action[] = [];
     while (true) {
-        const text = prompt('<user>');
-        print(text);
-        if (text === 'quit') break;
+        const a = actions.pop();
+        if (!a) {
+            const text = prompt('<user>') || '';
+            const event: Event = { type: 'message', text };
+            actions.unshift(...bot(event));
+            continue;
+        }
+        if (a.type === 'quit') break;
+        if (a.type === 'say') console.log(`<bot> ${a.text}`);
     }
 }
 
-if (import.meta.main) main();
+if (import.meta.main) {
+    runConsole(pingBot);
+}
